29c091da2b885b1f193bce965c6e71a4242d267d
张晓东(10029037) <zhangxd-i@glodon.com>
2022-04-28 19:26:59 +0800

    fix: 模板修改

--- src/main.js
+++ src/main.js
@@ -2,18 +2,21 @@
  * @Author: zhangxd-i
  * @Date: 2022-04-15 16:57:24
  * @LastEditors: zhangxd-i
- * @LastEditTime: 2022-04-15 17:30:57
+ * @LastEditTime: 2022-04-28 19:24:04
  * @Descripttion:
  */
 import Vue from 'vue'
 import App from './App.vue'
 import router from './router'
 import store from './store'
+{{#if elementGui}}
+  import ElementUI from 'element-gui'
+  import 'element-gui/lib/theme-chalk/index.css'
+  Vue.use(ElementUI)
+{{/if}}
 
-import ElementUI from 'element-gui'
-import 'element-gui/lib/theme-chalk/index.css'
 
-Vue.use(ElementUI)
+
 Vue.config.productionTip = false
 
 new Vue({
